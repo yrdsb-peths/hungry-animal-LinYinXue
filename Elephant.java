b5493096f764157dba436a1ef3d140b663964105
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-05-02 23:19:36 -0400

    Add method gameOver and label

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,26 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        eat();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
     }
 }
