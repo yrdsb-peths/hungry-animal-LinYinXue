0f7be7ed149d6dc1df13fc82024f3487412a6a1b
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-05-05 14:21:03 -0400

    add simpletimer

--- MyWorld.java
+++ MyWorld.java
@@ -21,7 +21,7 @@ public class MyWorld extends World
         super(600, 400, 1, false);
         
         Elephant elephant = new Elephant ();
-        addObject (elephant, 300, 250);
+        addObject (elephant, 300, 300);
         
         scoreLabel = new Label(0,80);
         addObject(scoreLabel,50,50);
