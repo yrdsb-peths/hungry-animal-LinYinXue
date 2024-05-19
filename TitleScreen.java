bc0f37e98e64ebd59bad3f79d215e2755e3a3688
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-05-19 14:18:16 -0400

    added title screen image

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,6 +17,10 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        GreenfootImage bgImage = new GreenfootImage("Title.png");
+        bgImage.scale(getWidth(), getHeight());
+        setBackground(bgImage);
+        
         addObject(titleLabel, getWidth()/2, 90);
         prepare();
     }
